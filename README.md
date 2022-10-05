3bf6457d1da3637961b4f6ee389c0a8dd341e46d
CodingWithDude <codingwithdude@gmail.com>
2022-10-05 15:17:08 -0700

    Inital Commit
    Bootstrap T3 project with Next.js Tailwind and TypeScript

---
+++ README.md
@@ -0,0 +1,1 @@
+Coding with Dude Portfolio project
---
+++ src/pages/index.tsx
@@ -0,0 +1,19 @@
+import type { NextPage } from "next";
+import Head from "next/head";
+
+const Home: NextPage = () => {
+  return (
+    <>
+      <Head>
+        <title>Coding with Dude</title>
+        <meta
+          name="description"
+          content="Coding with Dude Portfolio by Ryan Austin"
+        />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+    </>
+  );
+};
+
+export default Home;
