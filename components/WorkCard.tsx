e1f0e71eeb18945f14960ee5f269c82c63e3147a
CodingWithDude <codingwithdude@gmail.com>
2022-10-31 22:25:27 -0700

    fixed image layout on WorkCard

--- components/WorkCard.tsx
+++ components/WorkCard.tsx
@@ -34,9 +34,7 @@ const WorkCard = ({ project, index }: Props) => {
               <div className="absolute top-0 bottom-0 left-0 right-0 md:hidden">
                 <Image
                   src={project.image}
-                  layout="intrinsic"
-                  width="100%"
-                  height="100%"
+                  layout="fill"
                   objectFit="cover"
                   objectPosition="center"
                   alt=""
