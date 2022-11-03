1373eb99f2118852e930bbd3d7058539e210d9e2
CodingWithDude <codingwithdude@gmail.com>
2022-11-02 18:11:12 -0700

    updated blogProject picture

--- utils/constants.tsx
+++ utils/constants.tsx
@@ -23,11 +23,11 @@ export const navLinks = [
 
 const blogProject = {
   image:
-    "https://res.cloudinary.com/djjxydn3p/image/upload/ar_16:9,c_fill/v1667381772/portfolio/blog-bg_nc1vqr.png",
+    "https://res.cloudinary.com/djjxydn3p/image/upload/v1667437768/portfolio/blog_updated_gelvd9.png",
   // image:
   //   "https://res.cloudinary.com/djjxydn3p/image/upload/v1666750237/cld-sample-3.jpg",
   title: "CodingWithDude Blog",
-  description: `My digital playground where I share what I'm learning about exciting new technologies and building a career as a self taught developer.`,
+  description: `My blog built in Next.js with MDX Syntax Highlighting from rehype-pretty-code for code blocks, this is where I share what I am learning. Built with content-layer, TypeScript and styled with tailwind.`,
   technologies: [
     "TypeScript",
     "Content Layer",
